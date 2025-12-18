822d2f23af52f68486a679802c445e348327edc9
worm <worm@nowhere.jez>
2025-12-18 06:02:38 +0000

    Add PoC for PoW

---
+++ Poc/4-pow/src/main.rs
@@ -0,0 +1,76 @@
+use randomx_rs::*;
+
+/*
+Challenge Format: 128-bit
+     1 byte  difficulty (number of starting zero bits) (only 6-bits are used)
+    15 bytes random bits
+
+Solution Format: 64-bit
+    8 bytes salt
+*/
+
+fn create_challenge(difficulty: u8) -> [u8; 16] {
+    let mut buf = [0u8; 16];
+    getrandom::fill(&mut buf).unwrap();
+    buf[0] = difficulty&0b111111;
+
+    buf
+}
+
+fn validate_solution(vm: &RandomXVM, challenge: [u8; 16], solution: u64) -> bool {
+    let mut cat = [0u8; 24];
+    cat[..16].copy_from_slice(&challenge);
+    cat[16..].copy_from_slice(&solution.to_le_bytes());
+
+    let hash = vm.calculate_hash(&cat).unwrap();
+    let hash_u128 = u128::from_le_bytes(hash[0..16].try_into().unwrap()) ^ u128::from_le_bytes(hash[16..32].try_into().unwrap());
+
+    let difficulty = challenge[0]&0b111111;
+
+    hash_u128.leading_zeros() > difficulty.into()
+}
+
+fn solve_challenge(vm: &RandomXVM, challenge: [u8; 16]) -> u64 {
+    let mut solution: u64 = 0;
+    loop {
+        if validate_solution(vm, challenge, solution) { return solution };
+
+        solution += 1;
+    }
+}
+
+fn main() {
+    let cache = RandomXCache::new(RandomXFlag::FLAG_JIT | RandomXFlag::FLAG_ARGON2_AVX2, &[0]).unwrap();
+
+    println!("initializing dataset (only needs to be done once, at node startup)...");
+    let now = std::time::Instant::now();
+    let dataset = RandomXDataset::new(RandomXFlag::FLAG_DEFAULT, cache, 0).unwrap();
+    println!("initialized, took {:.2?}\n", now.elapsed());
+
+    let vm = RandomXVM::new(RandomXFlag::FLAG_HARD_AES | RandomXFlag::FLAG_FULL_MEM | RandomXFlag::FLAG_JIT, None, Some(dataset)).unwrap();
+     
+    for d in 0..15 {
+        let challenge = create_challenge(d);
+        println!("created challenge of difficulty {}", d);
+
+        let now = std::time::Instant::now();
+        println!("solving challenge {:02X?}", challenge);
+        let solution = solve_challenge(&vm, challenge);
+        println!("took {:.2?} ({} tries) to find solution", now.elapsed(), solution+1);
+        
+        let now = std::time::Instant::now();
+        assert!(validate_solution(&vm, challenge, solution));
+        println!("took {:.2?} to validate solution\n", now.elapsed());
+    }
+
+    let challenge = create_challenge(40);
+    println!("created challenge of difficulty 40");
+
+    let solution = getrandom::u64().unwrap();
+    println!("created random solution {}", solution);
+    
+    let result = validate_solution(&vm, challenge, solution);
+
+    assert!(result == false);
+    println!("solution correctly validated as {} (false)", result);
+}

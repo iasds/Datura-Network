74f8cc1d5af0eb59a8b684dfe387da98a81461f2
Midas <midas@dev.null>
2025-12-24 13:19:36 +0100

    propose a unified rust toolchain for development

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      fenix,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        toolchain = fenix.packages.${system}.minimal.toolchain;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              (fenix.packages.${system}.complete.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ])
+            ];
+
+            shellHook = ''
+              export CARGO_HOME="$PWD/.cargo"
+              export PATH="$CARGO_HOME/bin:$PATH"
+              mkdir -p .cargo
+              echo '*' > .cargo/.gitignore
+            '';
+          };
+        };
+      }
+    );
+}

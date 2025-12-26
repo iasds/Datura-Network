59667e146d0c8435163c83ba7bd6ff62e80671f0
Midas <midas@dev.null>
2025-12-26 18:48:57 +0100

    retrieve new jobs and cache the latest

--- Poc/4.7-p2pool-rx-challenges/src/lib/mod.rs
+++ Poc/4.7-p2pool-rx-challenges/src/lib/mod.rs
@@ -14,7 +14,7 @@ pub enum JobType {
 pub struct Client {
     addr: Option<String>,
     stream: Option<BufReader<TcpStream>>,
-    rq_id: i64,
+    last_job: Option<JobData>,
 }
 
 #[derive(Default, Serialize, Deserialize)]
@@ -43,8 +43,8 @@ struct Job {
     status: String,
 }
 
-#[derive(Deserialize)]
-struct JobData {
+#[derive(Deserialize, Clone, Debug)]
+pub struct JobData {
     blob: String,
     job_id: String,
     target: String,
@@ -61,16 +61,53 @@ enum ServerReply {
         id:i64,
         error: Option<String>,
         result: Job,
+    },
+    WorkOrder {
+        jsonrpc: String,
+        method: String,
+        params: JobData,
     }
 }
 
 
 impl Client {
+    pub async fn get_challenge(&mut self) -> Option<JobData> {
+        let mut line = String::new();
+        if let Some(reader) = &mut self.stream {
+            match tokio::time::timeout(std::time::Duration::from_secs(5), reader.read_line(&mut line)).await {
+                Ok(Ok(0)) => {
+                    println!("Server closed connection");
+                    self.last_job.clone()
+                }
+                Ok(Ok(_)) => {
+                    if let Ok(ServerReply::WorkOrder { params, .. }) = serde_json::from_str::<ServerReply>(&line) {
+                        self.last_job = Some(params.clone());
+                        self.last_job.clone()
+                    }
+                    else {
+                        panic!("bad reply type");
+                    }
+                }
+                Ok(Err(e)) => {
+                    panic!("Read error: {}", e);
+                }
+                Err(_) => {
+                    // timeout
+                    println!("No new job received in 5 seconds");
+                    self.last_job.clone()
+                }
+            }
+        } else {
+            todo!("return some dummy random job");
+            None
+        }
+
+    }
     pub async fn new(addr: Option<String>) -> Result<Self, ()> {
         let mut client = Client {
             addr,
             stream: None,
-            rq_id: 1,
+            last_job: None,
         };
         if let Some(ref ip_addr) = client.addr {
             let mut reader = BufReader::new(
@@ -79,13 +116,15 @@ impl Client {
                     .expect("Connection failed"),
             );
             let login_str = serde_json::to_string(&StratumQueries::Login{id: 1, jsonrpc: "2.0".to_string(), method: "login".to_string(), params: LoginParams::default() }).unwrap();
-            println!("{}",login_str);
             reader.get_mut().write_all(format!("{}\n",login_str).as_bytes()).await.unwrap();
-
             let mut line = String::new();
             let res = reader.read_line(&mut line).await.unwrap();
-            println!("subscribe response: {}, {} long", line, res);
-            let reply : ServerReply = serde_json::from_str(&line).unwrap();
+            if let ServerReply::LoginReply{result,..} = serde_json::from_str(&line).unwrap() {
+                client.last_job = Some(result.job);
+            }
+            else {
+                panic!("no job data in reply!");
+            }
             client.stream = Some(reader);
         }
        
--- Poc/4.7-p2pool-rx-challenges/src/main.rs
+++ Poc/4.7-p2pool-rx-challenges/src/main.rs
@@ -1,5 +1,6 @@
 
 #[tokio::main]
 async fn main() {
-    xmr_pow_challenges::Client::new(Some("127.0.0.1:3355".to_string())).await.unwrap();
+    let mut client = xmr_pow_challenges::Client::new(Some("127.0.0.1:3355".to_string())).await.unwrap();
+    println!("{:?}", client.get_challenge().await);
 }

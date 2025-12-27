ce17cb86325035bc810941ea222ba5a8f56616c5
Midas <midas@dev.null>
2025-12-27 22:20:57 +0100

    fix call to get_challenge

--- Poc/4.7-p2pool-rx-challenges/src/main.rs
+++ Poc/4.7-p2pool-rx-challenges/src/main.rs
@@ -3,7 +3,7 @@ async fn main() {
     let mut client = xmr_pow_challenges::Client::new(Some("127.0.0.1:3355".to_string()))
         .await
         .unwrap();
-    while let Some(result) = client.get_challenge().await {
+    while let Ok(result) = client.get_challenge().await {
         println!("got challenge {:?}", result);
     }
 }

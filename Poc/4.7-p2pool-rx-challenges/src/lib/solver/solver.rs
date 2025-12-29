d3718eb3275225866dc4da78f70bebd97e40dada
Midas <midas@dev.null>
2025-12-29 20:30:43 +0100

    start reworking the solver to use a proper threadpool

--- Poc/4.7-p2pool-rx-challenges/src/lib/solver/solver.rs
+++ Poc/4.7-p2pool-rx-challenges/src/lib/solver/solver.rs
@@ -20,24 +20,18 @@ pub struct Solver {
     ///fast mode we use one light worker for verifications (or hashing if it has nothing else to
     ///do) and the others for everything else
     mode: SolverMode,
-
-    ///if configured to run into more than 1 thread then other workers will be added
-    ///those workers can be light workers by default, or fast depending on the mode used
-    solvers: Vec<Worker>,
-
-    ///We always have at least 1 light mode worker
-    verifier: Worker,
-    ///Used to send SolverJob such as PoW to the solver
-    pub feed_route: mpsc::Sender<SolverJob>,
+    pub solver_input: mpsc::Sender<SolverJob>,
 
     ///Used by the solver tasks to receive new solverjobs
     receiver: mpsc::Receiver<SolverJob>,
 
-    ///Used by the workers to send back results
-    worker_results: mpsc::Receiver<SolverResult>,
-
     ///Used by the solver to send back results to the outside
     pub solver_output: mpsc::Sender<SolverResult>,
+    
+    ///used by the solver when a share can be sent back to the pool
+    upstream_pool: mpsc::Sender<SolverResult>,
+
+    workers: ThreadPool,
 }
 
 #[derive(Copy,Debug,Clone,PartialEq)]
@@ -49,40 +43,28 @@ pub enum SolverMode {
 impl Solver {
     ///If creating with multiple threads one will be a dedicated verification thread and priorize
     ///verification work
-    pub fn new(mode: SolverMode, mut nb_threads: usize, solver_output: mpsc::Sender<SolverResult>) -> Result<Self,SolverError> {
+    pub fn new(mode: SolverMode, mut nb_threads: usize, solver_output: mpsc::Sender<SolverResult>, upstream_pool: mpsc::Sender<SolverResult>) -> Result<Self,SolverError> {
         nb_threads = min(nb_threads,num_cpus::get());
         let flags = get_flags(mode);
-        let cache = Arc::new(RwLock::new(RandomXCache::new(flags,&[0u8;32])?)); //always at least one worker thread
+        let cache = RandomXCache::new(flags,&[0u8;32])?; //always at least one worker thread
         let dataset = if mode==SolverMode::Fast {
             let locked_cache = cache.read().unwrap();
-            Some(Arc::new(RwLock::new(RandomXDataset::new(flags, locked_cache.clone(),0)?)))
+            RandomXDataset::new(flags, cache.clone(),0)?
         }
         else {
             None
         };
 
+        //create rayon threadpool here, create 1 vm of the right type depending on the mode
 
-        let seed = Arc::new(RwLock::new([0u8;32]));
-        let (worker_input, worker_results) = mpsc::channel(WORKER_CHANNEL_SIZE);
-        let verifier = Worker::new(None, Some(cache.clone()), seed.clone(), worker_input.clone())?;
-        let mut solvers = Vec::new();
-
-        for _ in 0..nb_threads - 1 {
-            solvers.push(Worker::new(dataset.clone(),Some(cache.clone()),seed.clone(), worker_input.clone())?);
-        }
-
-
-
-        let (feed_route, receiver) = mpsc::channel(SOLVER_CHANNEL_SIZE);
+        let (solver_input, receiver) = mpsc::channel(SOLVER_CHANNEL_SIZE);
         Ok(Solver {
             mode,
-            solvers,
-            verifier,
-            feed_route,
+            solver_input
             receiver,
-            worker_results,
             solver_output,
+            upstream_pool,
+            pool: ThreadPool::new
         })
-
     }
 }
--- Poc/4.7-p2pool-rx-challenges/src/lib/solver/worker/tasks.rs
+++ Poc/4.7-p2pool-rx-challenges/src/lib/solver/worker/tasks.rs
@@ -1,45 +1,58 @@
 use crate::solver::{SolverJob, SolverResult};
+use super::WorkerError;
 use crate::solver::utils::hash_to_difficulty;
 use std::sync::{Arc,RwLock};
 use tokio::sync::mpsc;
-use std::time::Duration;
+use std::time::{Instant,Duration};
+use crate::SolverError;
 use randomx_rs::*;
 
-fn do_work(mut job_receiver: mpsc::Receiver<SolverJob>, result_sender: mpsc::Sender<SolverResult>,dataset: Option<Arc<RwLock<RandomXDataset>>>,cache: Option<Arc<RwLock<RandomXCache>>>,seed: Arc<RwLock<[u8;32]>>, flags: RandomXFlag) {
+
+fn do_work(mut job_receiver: mpsc::Receiver<SolverJob>, result_sender: mpsc::Sender<SolverResult>, vm: &RandomXVM) {
     let mut worst_job_duration = Duration::from_millis(0);
     let mut worst_verify_duration = Duration::from_millis(0);
     loop {
         while let Some(job) = job_receiver.blocking_recv() {
-            match job {
-                Verify((pow,solution_candidate)) => {
-                    let difficulty = hash_to_difficulty(solution_candidate.as_slice().into());
+            let (pow,solution_candidate, time_allotment) = match job {
+                SolverJob::Verify((pow,solution_candidate)) => {
+                    let difficulty = hash_to_difficulty(solution_candidate.as_slice().try_into().unwrap());
                     if difficulty < pow.target {
                         result_sender.blocking_send(SolverResult::Invalid((pow,WorkerError::LowDifficultyShare.into())));
                         continue;
                     }
 
                     if let Err(err) = update_ro_data(dataset, cache, seed,flags, &pow.seed_hash) {
-                        result_sender.blocking_send(SolverError::RandomXError(err));
+                        result_sender.blocking_send(SolverResult::Error(SolverError::RandomXError(err)));
                         continue;
                     }
-                    let work_start = Instant::now();
+                    (Some(pow),Some(solution_candidate),None)
+                }
+                SolverJob::Solve((mut pow,alloted_time)) => {
+                    if let Err(err) = update_ro_data(dataset, cache, seed, flags,&pow.seed_hash) {
+                        result_sender.blocking_send(SolverResult::Error(SolverError::RandomXError(err)));
+                        continue;
+                    }
+                    (Some(pow),None,Some(alloted_time))
+
+                }
+            };
                     let vm = if cache.is_some() {
                         let cache = cache.unwrap().read().unwrap();
                         let dataset = dataset.unwrap().read().unwrap();
                         match RandomXVM::new(flags, Some(*cache), Some(*dataset)) {
                             Ok(vm) => vm,
                             Err(some_err) => {
-                            result_sender.blocking_send(SolverError::RandomXError(some_err));
+                            result_sender.blocking_send(SolverResult::Error(SolverError::RandomXError(some_err)));
                             continue;
                             }
                         }
-                    } else if dataset.is_some() {
-                        let dataset = dataset.unwrap().read().unwrap();
-                        match RandomXVM::new(flags,None,Some(*dataset)) {
+                    } else if let Some(set) = dataset {
+                        let rl_dataset = set.read().unwrap();
+                        match RandomXVM::new(flags,None,Some(*rl_dataset)) {
                             Ok(vm) => vm,
                             Err(some_err) => {
 
-                            result_sender.blocking_send(SolverError::RandomXError(some_err));
+                            result_sender.blocking_send(SolverResult::Error(SolverError::RandomXError(some_err)));
                             continue;
                             }
                         }
@@ -47,43 +60,50 @@ fn do_work(mut job_receiver: mpsc::Receiver<SolverJob>, result_sender: mpsc::Sen
                         panic!("I have neither cache nor dataset!");
                     };
 
-                    match vm.calculate_hash(&pow.blob) {
-                        Ok(solution) => {
-                            if solution_candidate != solution {
-                                result_sender.blocking_send(SolverResult::Invalid((pow,WorkerError::InvalidShare.into())));
-                                continue;
-                            }
-                            result_sender.blocking_send(SolverResult::Valid(pow));
-                            let work_time = work_start.elapsed();
-                            if work_time > worst_verify_duration {
-                                worst_verify_duration = work_time;
-                            }
-                        },
-                        Err(some_err) => {
-                            result_sender.blocking_send(SolverResult::Error(some_err.into()));
-                        }
-                    }
-                }
-                Solve((mut pow,alloted_time)) => {
+
                     let work_start = Instant::now();
-                    if let Err(err) = update_ro_data(dataset, cache, seed, flags,&pow.seed_hash) {
-                        result_sender.blocking_send(some_err.into());
-                        continue;
-                    }
-                    let mut best_solution = (Vec::new(),1u64);
-                    while work_start.elapsed() < alloted_time {
-                        challenge.new_nonce();
-                        let solution = vm.calculate_hash(&pow.blob)?;
-                        let difficulty = hash_to_difficulty(&solution_candidate.as_slice());
-                        if difficulty > pow.target && difficulty > best_solution.1 {
-                            best_solution = (solution,difficulty);
+                    match (pow,solution_candidate,time_allotment) {
+                        (Some(mut pow),None,Some(alloted_time)) => {
+                                let mut best_solution = (Vec::new(),1u64);
+                                while work_start.elapsed() < alloted_time {
+                                    pow.new_nonce();
+                                    let solution = vm.calculate_hash(&pow.blob).unwrap();
+                                    let difficulty = hash_to_difficulty(solution.as_slice().try_into().unwrap());
+                                    if difficulty > pow.target && difficulty > best_solution.1 {
+                                        best_solution = (solution,difficulty);
+                                    }
+                                }
+                                let total = work_start.elapsed();
+                                if total > worst_job_duration {
+                                    worst_job_duration = total;
+                                }
+                                if best_solution.1 >= pow.target {
+                                    result_sender.blocking_send(SolverResult::Solved((pow,best_solution.0)));
+                                }
                         }
+                        (Some(pow),Some(solution_candidate),None) => {
+
+
+                                match vm.calculate_hash(&pow.blob) {
+                                    Ok(solution) => {
+                                        if solution_candidate != solution {
+                                            result_sender.blocking_send(SolverResult::Invalid((pow,WorkerError::InvalidShare.into())));
+                                            continue;
+                                        }
+                                        result_sender.blocking_send(SolverResult::Valid((pow,solution_candidate)));
+                                        let work_time = work_start.elapsed();
+                                        if work_time > worst_verify_duration {
+                                            worst_verify_duration = work_time;
+                                        }
+                                    },
+                                    Err(some_err) => {
+                                        result_sender.blocking_send(SolverResult::Error(some_err.into()));
+                                    }
+                                }
+                        }
+                        _ => panic!("misparsing of task"),
+
                     }
-                    if best_solution.1 >= pow.target {
-                        result_sender.blocking_send(SolverResult::Solved((pow,best_solution.0)));
-                    }
-                }
-            }
         }
     }
 }

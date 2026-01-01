fa657471babc462ce75c9cb763e5f324dd58a7e0
Midas <midas@dev.null>
2026-01-01 13:25:28 +0100

    add timing and pow lifetime constants

--- Poc/4.7-p2pool-rx-challenges/src/lib/consts.rs
+++ Poc/4.7-p2pool-rx-challenges/src/lib/consts.rs
@@ -5,3 +5,15 @@ pub const NONCE_SIZE: usize = 4;
 pub const EXPECTED_DELIVERY_LATENCY: Duration = Duration::from_millis(500);
 pub const SOLVER_CHANNEL_SIZE: usize = 64;
 pub const WORKER_CHANNEL_SIZE: usize = 16;
+pub const VERIFY_USUAL_DURATION: Duration = Duration::from_millis(50);
+
+
+/// pow max lifetime, for random pows they will have an expiration between 0 and this lifetime
+/// client job list is also cleaned up based on this lifetime
+pub const POW_MAX_LIFETIME: Duration = Duration::from_secs(5);
+
+/// random seed_hash max lifetime when used for genrating new pows
+pub const SEED_LIFETIME: Duration = Duration::from_hours(48);
+
+/// minimal difficulty for new client pows
+pub const MINIMAL_DIFFICULTY: u64 = 200; //to test

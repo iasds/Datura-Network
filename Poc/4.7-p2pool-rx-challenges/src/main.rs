84e376db74853c83c8559164677e482c7b606405
Midas <midas@dev.null>
2026-01-02 16:00:06 +0100

    high power fast test

--- Poc/4.7-p2pool-rx-challenges/src/main.rs
+++ Poc/4.7-p2pool-rx-challenges/src/main.rs
@@ -1,8 +1,8 @@
 use std::ops::Add;
 use tokio::sync::mpsc;
 use tokio::task::spawn;
-use tokio::time::{timeout,Duration, Instant, sleep};
-use xmr_pow_challenges::{consts,Client, DaturaPow, Solver, SolverJob, SolverMode, SolverResult};
+use tokio::time::{Duration, Instant, sleep, timeout};
+use xmr_pow_challenges::{Client, DaturaPow, Solver, SolverJob, SolverMode, SolverResult, consts};
 
 #[tokio::main]
 async fn main() {
@@ -11,8 +11,8 @@ async fn main() {
     let (upstream_pool_sender, upstream_pool_receiver) = mpsc::channel(1);
     println!("creating a single thread light solver");
     let mut solver = Solver::new(
-        SolverMode::Light,
-        4,
+        SolverMode::Fast,
+        6,
         solver_input_receiver,
         solver_output_sender,
         upstream_pool_sender,
@@ -32,20 +32,19 @@ async fn main() {
         let now = Instant::now();
         let job = Client::get_solver_job(local_client.clone()).await;
         solver_input_sender.send(job.clone()).await;
-        match timeout(consts::POW_MAX_LIFETIME,solver_output_receiver.recv()).await {
+        match timeout(consts::POW_MAX_LIFETIME, solver_output_receiver.recv()).await {
             Err(_) => {}
             Ok(result) => {
-                    if let SolverResult::Solved((pow, solution)) = result.unwrap()
-                    {
-                        println!("checking solution");
-                        solver_input_sender
-                            .send(SolverJob::Verify((
-                                pow,
-                                solution,
-                                Instant::now().add(Duration::from_secs(5)).into(),
-                            )))
-                            .await;
-                    }
+                if let SolverResult::Solved((pow, solution)) = result.unwrap() {
+                    println!("checking solution");
+                    solver_input_sender
+                        .send(SolverJob::Verify((
+                            pow,
+                            solution,
+                            Instant::now().add(Duration::from_secs(5)).into(),
+                        )))
+                        .await;
+                }
             }
         }
         println!("\n");

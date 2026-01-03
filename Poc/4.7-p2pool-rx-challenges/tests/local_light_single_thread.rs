b4b7588bb5c3499993bbff27450e00f7aaef6ffd
Midas <midas@dev.null>
2026-01-03 11:31:16 +0100

    update single thread test

--- Poc/4.7-p2pool-rx-challenges/tests/local_light_single_thread.rs
+++ Poc/4.7-p2pool-rx-challenges/tests/local_light_single_thread.rs
@@ -5,30 +5,30 @@ use tokio::time::{Duration, Instant, timeout};
 use xmr_pow_challenges::{Client, Solver, SolverJob, SolverMode, SolverResult, consts};
 
 #[tokio::main]
-async fn main() {
+#[test]
+async fn run_single_thread_light_10_pows() {
     let (solver_input_sender, solver_input_receiver) = mpsc::channel(1);
     let (solver_output_sender, mut solver_output_receiver) = mpsc::channel(1);
-    let (upstream_pool_sender, upstream_pool_receiver) = mpsc::channel(1);
     println!("creating a single thread light solver");
     let solver = Solver::new(
         SolverMode::Light,
         1,
         solver_input_receiver,
         solver_output_sender,
-        upstream_pool_sender,
+        None,
     )
     .unwrap();
     spawn(Solver::do_work(solver.clone()));
 
     println!("creating client for local pow gen");
 
-    let local_client = Client::new(Some("127.0.0.1:3355".to_string()), upstream_pool_receiver)
+    let local_client = Client::new(None, None)
         .await
         .unwrap();
     spawn(Client::start(local_client.clone()));
 
     println!("solving jobs as fast as we can (each . is a new job, is $ is a valid solution produced");
-    loop {
+    for _ in 0..10 {
         let _now = Instant::now();
         let job = Client::get_solver_job(local_client.clone()).await;
         print!(".");

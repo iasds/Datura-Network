c1a969b8e3ce30fa673b8235763f3c58b436c33b
Ziak <ziak@obviouslyfalse.email>
2026-01-17 18:24:53 +0100

    feat(throttling)[server]: convert server to a standalone function

--- Poc/6-throttling/src/bin/server.rs
+++ Poc/6-throttling/src/bin/server.rs
@@ -3,13 +3,11 @@ use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use leaky_bucket::RateLimiter;
 use std::collections::HashMap;
-use std::env;
 use std::error::Error;
 use std::net::IpAddr;
 use std::sync::{Arc, Mutex};
 
-const DEFAULT_ADDR: &str = "127.0.0.1";
-const DATA_PORT: &str = "9977";
+const DATA_ADDR: &str = "127.0.0.1:9977";
 const BUFFER_SIZE: usize = 4096;
 
 const DEFAULT_BANDWIDTH: usize = 10 * 1024; // 10kb
@@ -17,34 +15,26 @@ const DEFAULT_BANDWIDTH: usize = 10 * 1024; // 10kb
 type NodeID = IpAddr;  // node are identified by their ip address
 
 // inspired from https://github.com/tokio-rs/tokio/blob/master/examples/echo-tcp.rs
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| DEFAULT_ADDR.to_string()) + ":" + DATA_PORT;
-
-    let listener = TcpListener::bind(&addr).await?;
-
-    let limiters: Arc<Mutex<HashMap<NodeID, Arc<RateLimiter>>>> = Arc::new(Mutex::new(HashMap::new()));
+async fn data_thread(
+    limiters: Arc<Mutex<HashMap<NodeID, Arc<RateLimiter>>>>
+) -> Result<(), Box<dyn Error>>  {
+    let listener = TcpListener::bind(DATA_ADDR).await?;
 
     loop {
-        let (mut socket, addr) = listener.accept().await?;
+	let (mut socket, addr) = listener.accept().await.unwrap();
 	let mut stdout = tokio::io::stdout();
 	let limiters = limiters.clone();
 
-        tokio::spawn(async move {
-            let mut buf = vec![0; BUFFER_SIZE];
+	tokio::spawn(async move {
+	    let mut buf = vec![0; BUFFER_SIZE];
 
-            loop {
-                match socket.read(&mut buf).await {
-                    Ok(0) => {
-			// closed by peer
-                        return;
-                    }
-                    Ok(n) => {
+	    loop {
+		match socket.read(&mut buf).await {
+		    Ok(0) => { return; }
+		    Ok(n) => {
 			// write to the standard output. if writing fails, log and exit.
-                        if let Err(e) = stdout.write_all(&buf[0..n]).await {
-                            eprintln!("Failed to write to socket {}: {}", addr, e);
+			if let Err(e) = stdout.write_all(&buf[0..n]).await {
+			    eprintln!("Failed to write to socket {}: {}", addr, e);
 			    return;
 			}
 			let limiter = limiters
@@ -66,12 +56,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 			limiter.acquire(n).await;
 		    }
-                    Err(e) => {
-                        eprintln!("Failed to read from socket {}: {}", addr, e);
-                        return;
-                    }
-                }
-            }
-        });
+		    Err(e) => {
+			eprintln!("Failed to read from socket {}: {}", addr, e);
+			return;
+		    }
+		}
+	    }
+	});
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let limiters: Arc<Mutex<HashMap<NodeID, Arc<RateLimiter>>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    data_thread(limiters).await
 }

247b0461ba7bfa07c716f8f0dad5cfdf8b03ec19
Ziak <ziak@obviouslyfalse.email>
2026-01-17 15:50:52 +0100

    feat(throttling)[server]: introducing rate limiting

--- Poc/6-throttling/src/bin/server.rs
+++ Poc/6-throttling/src/bin/server.rs
@@ -1,12 +1,16 @@
+use tokio::time::Duration;
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use leaky_bucket::RateLimiter;
 use std::env;
 use std::error::Error;
+use std::sync::Arc;
 
 const DEFAULT_ADDR: &str = "127.0.0.1";
 const DATA_PORT: &str = "9977";
 const BUFFER_SIZE: usize = 4096;
 
+const DEFAULT_BANDWIDTH: usize = 10 * 1024; // 10kb
 
 // inspired from https://github.com/tokio-rs/tokio/blob/master/examples/echo-tcp.rs
 #[tokio::main]
@@ -20,6 +24,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loop {
         let (mut socket, addr) = listener.accept().await?;
 	let mut stdout = tokio::io::stdout();
+
+	// 10kb rate limiter.
+	let limiter = Arc::new(
+	    RateLimiter::builder()
+		.initial(DEFAULT_BANDWIDTH)
+		.max(DEFAULT_BANDWIDTH)
+		.refill(DEFAULT_BANDWIDTH / 100)
+		.interval(Duration::from_millis(10))
+		.build()
+	);
 
         tokio::spawn(async move {
             let mut buf = vec![0; BUFFER_SIZE];
@@ -36,7 +50,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             eprintln!("Failed to write to socket {}: {}", addr, e);
                             return;
                         }
-                    }
+			limiter.acquire(n).await;
+		    }
                     Err(e) => {
                         eprintln!("Failed to read from socket {}: {}", addr, e);
                         return;

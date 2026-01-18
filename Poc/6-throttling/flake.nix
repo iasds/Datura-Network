bad94847833a3f3eff93c6e7b7a5d4d181990e6a
Ziak <ziak@obviouslyfalse.email>
2026-01-19 00:25:09 +0100

    feat(throttling)[tooling]: add nix run

--- Poc/6-throttling/flake.nix
+++ Poc/6-throttling/flake.nix
@@ -11,8 +11,7 @@
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
 
-      in
-      {
+      in rec {
         defaultPackage = naersk-lib.buildPackage {
           src = ./.;
           nativeBuildInputs = with pkgs; [ cmake ];
@@ -21,6 +20,15 @@
           buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
+        apps.server = {
+          type = "app";
+          program = "${defaultPackage}/bin/server";
+        };
+        apps.client = {
+          type = "app";
+          program = "${defaultPackage}/bin/client";
+        };
+        apps.default = apps.server;
       }
     );
 }

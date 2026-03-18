f5d57a2cb234927ae2f1db416872809caba5f71a
Ziak <ziak@obviouslyfalse.email>
2026-03-18 14:14:24 +0100

    space-allocation(protocol): convert protocol from str

--- Poc/6.2-space-allocation/src/protocol.rs
+++ Poc/6.2-space-allocation/src/protocol.rs
@@ -1,6 +1,18 @@
+use std::str::FromStr;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Protocol {
 	Knock,
 	Put(usize),
 	Get([u8; 32]),
+}
+
+impl FromStr for Protocol {
+	type Err = ();
+	fn from_str(input: &str) -> Result<Protocol, Self::Err> {
+		match input {
+			"KNOCK" => Ok(Protocol::Knock),
+			_ => Err(()),
+		}
+	}
 }

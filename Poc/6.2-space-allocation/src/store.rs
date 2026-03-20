8c5962fe81846c50ae69f758bf6e337d6d244db0
Ziak <ziak@obviouslyfalse.email>
2026-03-20 09:12:23 +0100

    space-allocation(store): take a &mut reference to a socket

--- Poc/6.2-space-allocation/src/store.rs
+++ Poc/6.2-space-allocation/src/store.rs
@@ -4,7 +4,7 @@ const DATA_PATH: &str = "/tmp/datura/store";
 const BUFFER_SIZE: usize = 8192;
 
 // inspired from https://github.com/tokio-rs/tokio/blob/master/examples/echo-tcp.rs
-pub async fn read_from(mut socket: TcpStream, data_len: usize) {
+pub async fn read_from(socket: &mut TcpStream, data_len: usize) {
 	let mut buf = vec![0; BUFFER_SIZE];
 	todo!();
 }

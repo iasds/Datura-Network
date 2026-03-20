ac4b9f39ad57355958b4ffed082ae8f72221e1ff
Ziak <ziak@obviouslyfalse.email>
2026-03-20 15:39:38 +0100

    space-allocation(upload): send PUT request

--- Poc/6.2-space-allocation/src/bin/upload.rs
+++ Poc/6.2-space-allocation/src/bin/upload.rs
@@ -2,6 +2,8 @@
 use std::env;
 use std::error::Error;
 use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
 
 const CONTROL_ADDR: &str = "127.0.0.1:9978";
 
@@ -10,6 +12,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	let fname: String = env::args().next().unwrap();
 	let file = File::open(fname).await?;
 	let file_size = file.metadata().await?.len();
+
+	let mut stream = TcpStream::connect(CONTROL_ADDR).await.unwrap();
+
+	stream
+		.write_all(format!("PUT {}", file_size).as_bytes())
+		.await?;
 
 	Ok(())
 }

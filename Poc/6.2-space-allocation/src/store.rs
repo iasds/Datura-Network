5cde61fde3f7fce13d98606fb9743a9e1ad26985
Ziak <ziak@obviouslyfalse.email>
2026-03-20 09:33:48 +0100

    space-allocation(store): use tokio's io result

--- Poc/6.2-space-allocation/src/store.rs
+++ Poc/6.2-space-allocation/src/store.rs
@@ -1,11 +1,12 @@
 use rand::Rng;
+use tokio::io;
 use tokio::net::TcpStream;
 
 const DATA_PATH: &str = "/tmp/datura/store";
 const BUFFER_SIZE: usize = 8192;
 
 // inspired from https://github.com/tokio-rs/tokio/blob/master/examples/echo-tcp.rs
-pub async fn read_from(socket: &mut TcpStream, data_len: usize) -> Result<[u8; 32], ()> {
+pub async fn read_from(socket: &mut TcpStream, data_len: usize) -> io::Result<[u8; 32]> {
 	let mut id = [0u8; 32];
 	rand::rng().fill(&mut id);
 

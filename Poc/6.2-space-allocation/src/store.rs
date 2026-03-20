abec154a83d10e906bfefe0de926177579ad1849
Ziak <ziak@obviouslyfalse.email>
2026-03-20 09:22:18 +0100

    space-allocation(store): generate ID (first part of PUT request)

--- Poc/6.2-space-allocation/src/store.rs
+++ Poc/6.2-space-allocation/src/store.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use tokio::net::TcpStream;
 
 const DATA_PATH: &str = "/tmp/datura/store";
@@ -5,6 +6,9 @@ const BUFFER_SIZE: usize = 8192;
 
 // inspired from https://github.com/tokio-rs/tokio/blob/master/examples/echo-tcp.rs
 pub async fn read_from(socket: &mut TcpStream, data_len: usize) -> Result<[u8; 32], ()> {
+	let mut id = [0u8; 32];
+	rand::rng().fill(&mut id);
+
 	let mut buf = vec![0; BUFFER_SIZE];
 	todo!();
 }

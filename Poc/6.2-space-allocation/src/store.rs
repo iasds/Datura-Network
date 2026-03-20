0a921a1457aee7a8a8075e914b64de7b92a67cb3
Ziak <ziak@obviouslyfalse.email>
2026-03-20 19:00:41 +0100

    space-allocation(store): use the user's limiter for upload bandwidth

--- Poc/6.2-space-allocation/src/store.rs
+++ Poc/6.2-space-allocation/src/store.rs
@@ -1,9 +1,12 @@
+use crate::bandwidth::NodeRateLimiter;
 use fs2::available_space;
 use rand::Rng;
+use std::sync::Arc;
 use tokio::fs;
 use tokio::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use tokio::sync::Mutex;
 
 const DATA_PATH: &str = "/tmp/datura/store";
 const BUFFER_SIZE: usize = 8192;
@@ -24,7 +27,11 @@ pub fn difficulty(n: usize) -> u8 {
 }
 
 // inspired from https://github.com/tokio-rs/tokio/blob/master/examples/echo-tcp.rs
-pub async fn read_from(socket: &mut TcpStream, mut n: usize) -> io::Result<[u8; 32]> {
+pub async fn read_from(
+	socket: &mut TcpStream,
+	mut n: usize,
+	limiter: Arc<Mutex<NodeRateLimiter>>,
+) -> io::Result<[u8; 32]> {
 	let mut id = [0u8; 32];
 	rand::rng().fill(&mut id);
 
@@ -39,6 +46,7 @@ pub async fn read_from(socket: &mut TcpStream, mut n: usize) -> io::Result<[u8; 
 				let len = b.min(n);
 				file.write_all(&buf[0..len]).await?;
 				n -= len;
+				limiter.lock().await.bucket.acquire(len).await;
 			}
 			Err(e) => return Err(e),
 		}
